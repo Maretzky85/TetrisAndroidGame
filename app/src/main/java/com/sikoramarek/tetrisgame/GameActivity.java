8fae8a9349a0606ed1e08949088eb136c9a63559
Marek <34583771+Maretzky85@users.noreply.github.com>
2021-06-13 21:14:57 +0200

    Ui fixes (#1)
    * Added "No score yet" string if no score is saved into the device
    * Changed exit text to "Double click back to exit"
    * Changed window flags
    * Refactored and cleaned main classes

--- app/src/main/java/com/sikoramarek/tetrisgame/GameActivity.java
+++ app/src/main/java/com/sikoramarek/tetrisgame/GameActivity.java
@@ -1,11 +1,9 @@
 package com.sikoramarek.tetrisgame;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -21,13 +19,22 @@ public class GameActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        hideSystemUI();
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         GameView gameView = new GameView(this);
         setContentView(gameView);
         gameController = new GameController(gameView);
-        if (savedInstanceState != null){
+        if (savedInstanceState != null) {
             gameController.loadStateFrom(savedInstanceState);
         }
         new Thread(gameController).start();
@@ -49,11 +56,28 @@ public class GameActivity extends Activity {
     public void onBackPressed() {
         long timeNow = System.currentTimeMillis();
         long timePressed = timeNow - lastBackPressedTime;
-        if(timePressed < 500) {
+        if (timePressed < 500) {
             super.onBackPressed();
         } else {
-            Toast.makeText(getApplicationContext(), "Press back again to Exit", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), getString(R.string.exitPrompt), Toast.LENGTH_LONG).show();
             lastBackPressedTime = System.currentTimeMillis();
         }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
